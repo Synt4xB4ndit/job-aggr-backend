b711673362e23340ed6013c271cbab9962ca8f23
Nic Amaral <Namaral1995@gmail.com>
2026-06-12 23:31:09 -0400

    CORS update in app.py

--- app.py
+++ app.py
@@ -4,16 +4,25 @@ from fastapi.middleware.cors import CORSMiddleware
 from config import APP_NAME, APP_VERSION
 from routers.jobs import router as jobs_router
 
+import os
+
 app = FastAPI(
     title=APP_NAME,
     version=APP_VERSION
 )
 
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+origins = [
+    "http://localhost:3000",
+]
+
+if FRONTEND_URL:
+    origins.append(FRONTEND_URL)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
